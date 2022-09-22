220756785a9d8932b5bcebf9779baa830619305c
nickyjhong <nicolehong95@gmail.com>
2022-09-22 16:54:41 -0400

    leetcode fizzBuzz

--- leetcode/easy/1-twoSum.js
+++ leetcode/easy/1-twoSum.js
@@ -6,7 +6,6 @@
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-
 // Time: O(n)
 // Space: O(n)
 
---
+++ leetcode/easy/412-fizzBuzz.js
@@ -0,0 +1,27 @@
+// Given an integer n, return a string array answer (1-indexed) where:
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+
+// Input: n = 3
+// Output: ["1","2","Fizz"]
+
+// Time: O(n)
+// Space: O(1)
+
+const fizzBuzz = (num) => {
+  let result = [];
+  for (let i = 1; i <= num; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+          result.push('FizzBuzz')
+      } else if (i % 3 === 0) {
+          result.push('Fizz')
+      } else if (i % 5 === 0) {
+          result.push('Buzz')
+      } else {
+          result.push(i.toString())
+      }
+  }
+  return result;
+}
