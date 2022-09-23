b50a31c6a88264781eee228aee91f9d6359d015e
nickyjhong <nicolehong95@gmail.com>
2022-09-23 15:05:25 -0400

    leetcode fizzbuzz

--- leetcode/easy/412-fizzBuzz.js
+++ leetcode/easy/412-fizzBuzz.js
@@ -13,15 +13,15 @@
 const fizzBuzz = (num) => {
   let result = [];
   for (let i = 1; i <= num; i++) {
-      if (i % 3 === 0 && i % 5 === 0) {
-          result.push('FizzBuzz')
-      } else if (i % 3 === 0) {
-          result.push('Fizz')
-      } else if (i % 5 === 0) {
-          result.push('Buzz')
-      } else {
-          result.push(i.toString())
-      }
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i.toString());
+    }
   }
   return result;
-}
+};
