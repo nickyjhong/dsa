24054444e12372d8fe45ef554c1f29e9f9958eb4
nickyjhong <nicolehong95@gmail.com>
2022-10-02 12:35:32 -0400

    leetcode 14 longestCommonPrefix

---
+++ leetcode/easy/14-longestCommonPrefix.js
@@ -0,0 +1,22 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+
+// n = sum of all characters in all strings
+// Time: O(n) 
+// Space: O(1)
+
+const longestCommonPrefix = (strs) => {
+  let prefix = "";
+  for (let i = 0; i < strs[0].length; i++) {
+    let char = strs[0][i];
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] !== char) return prefix;
+    }
+    prefix = prefix + char;
+  }
+  return prefix;
+}
