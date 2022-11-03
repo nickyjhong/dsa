e0c169aec3023d079a2e7f242525f78503c08d5d
nickyjhong <nicolehong95@gmail.com>
2022-11-03 15:17:05 -0400

    leetcode 125 validPalindrome

--- leetcode/easy/125-validPalindrome.js
+++ leetcode/easy/125-validPalindrome.js
@@ -8,13 +8,31 @@
 // Explanation: "amanaplanacanalpanama" is a palindrome.
 
 // Time: O(n)
-// Space: O(n)
+// Space: O(1)
 
 const isPalindrome = (s) => {
-  if (s.length === 1) return true;
-  // only alphanumeric
-  // g = global (doesnt just stop after replacing once) i = case insensitive
-  let str = s.replace(/[^A-Z0-9]/gi, "").toLowerCase();
-  let reverseStr = str.toLowerCase().split("").reverse().join("")
-  return str === reverseStr
-}
+  s = s.toLowerCase();
+  s = s.replace(/[^A-Za-z0-9]/g, '');
+
+  let start = 0;
+  let end = s.length-1; 
+  
+  while (start < end){
+    if(s[start] !== s[end]) return false
+    start++;
+    end--;
+  }
+  return true
+};
+
+// Time: O(n)
+// Space: O(n)
+
+// const isPalindrome = (s) => {
+//   if (s.length === 1) return true;
+//   // only alphanumeric
+//   // g = global (doesnt just stop after replacing once) i = case insensitive
+//   let str = s.replace(/[^A-Z0-9]/gi, "").toLowerCase();
+//   let reverseStr = str.toLowerCase().split("").reverse().join("")
+//   return str === reverseStr
+// }
