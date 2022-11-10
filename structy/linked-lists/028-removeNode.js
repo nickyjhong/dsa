02dcd76a3a332e4f88ccbc995bf4856f74b20d7e
nickyjhong <nicolehong95@gmail.com>
2022-11-09 20:21:30 -0500

    structy 028 remove node

---
+++ structy/linked-lists/028-removeNode.js
@@ -0,0 +1,46 @@
+// Write a function, removeNode, that takes in the head of a linked list and a target value as arguments. 
+// The function should delete the node containing the target value from the linked list and return the head of the resulting linked list. 
+// If the target appears multiple times in the linked list, only remove the first instance of the target in the list.
+
+// Do this in-place.
+
+// You may assume that the input list is non-empty.
+
+// You may assume that the input list contains the target.
+
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+// const e = new Node("e");
+// const f = new Node("f");
+
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+// e.next = f;
+
+// a -> b -> c -> d -> e -> f
+
+// removeNode(a, "c"); // a -> b -> d -> e -> f
+
+// Time: O(n)
+// Space: O(1)
+
+const removeNode = (head, targetVal) => {
+  if (head.val === targetVal) return head.next;
+
+  let prev = null;
+  let current = head;
+  while (current !== null) {
+    if (current.val === targetVal) {
+      let next = current.next;
+      prev.next;
+      return head;
+    }
+    prev = current;
+    current = current.next;
+  }
+  return head;
+}
