f2a6762237b6c5a30c09fa5d1ef225f04cebc5b8
nickyjhong <nicolehong95@gmail.com>
2022-11-15 13:13:21 -0500

    commented out structy problems

--- structy/binary-tree/039-treeSum.js
+++ structy/binary-tree/039-treeSum.js
@@ -1,5 +1,5 @@
-Write a function, treeSum, that takes in the root of a binary tree that contains number values. 
-The function should return the total sum of all values in the tree.
+// Write a function, treeSum, that takes in the root of a binary tree that contains number values. 
+// The function should return the total sum of all values in the tree.
 
 // const a = new Node(3);
 // const b = new Node(11);
--- structy/binary-tree/040-treeIncludes.js
+++ structy/binary-tree/040-treeIncludes.js
@@ -1,5 +1,5 @@
-Write a function, treeIncludes, that takes in the root of a binary tree and a target value. 
-The function should return a boolean indicating whether or not the value is contained in the tree.
+// Write a function, treeIncludes, that takes in the root of a binary tree and a target value. 
+// The function should return a boolean indicating whether or not the value is contained in the tree.
 
 // const a = new Node("a");
 // const b = new Node("b");
