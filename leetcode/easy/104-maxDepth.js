f1ee0f2161fbfe7e7389089ed4aa7a481a6e40eb
nickyjhong <nicolehong95@gmail.com>
2022-11-21 18:32:06 -0500

    leetcode 104 maxDepth example

--- leetcode/easy/104-maxDepth.js
+++ leetcode/easy/104-maxDepth.js
@@ -9,6 +9,59 @@
 // Space: O(n)
 
 const maxDepth = (root) => {
+  // empty tree has a height of 0
   if (root === null) return 0
-  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+  const leftDepth = maxDepth(root.left)
+  const rightDepth = maxDepth(root.right)
+  return 1 + Math.max(leftDepth, rightDepth)
 }
+
+/*
+- Use recursion so the answer bubbles up from the bottom
+- Need to add 1 after each call so that the comparison moves up (or it'll just stay the same) 
+*/
+
+// maxDepth([a,b,c,null,null,d,e])
+
+//     a
+//   /   \
+//  b     c
+//       /  \
+//      d     e
+
+
+// b -> leftDepth = 0, rightDepth = 0 + 1 = 1
+//     a
+//   /   \
+//  1     c
+//       /  \
+//      d     e
+
+// d -> leftDepth = 0, rightDepth = 0 + 1 = 1
+//     a
+//   /   \
+//  1     c
+//       /  \
+//     1     e
+
+// e -> leftDepth = 0, rightDepth = 0 + 1 = 1
+//     a
+//   /   \
+//  1     c
+//       /  \
+//     1      1
+
+// c -> leftDepth = 1, rightDepth = 1 + 1 = 2
+//     a
+//   /   \
+//  1     2
+//       /  \
+//     1      1
+
+
+// a -> leftDepth = 1, rightDepth = 2 + 1 = 3
+//     3
+//   /   \
+//  1     2
+//       /  \
+//     1      1
