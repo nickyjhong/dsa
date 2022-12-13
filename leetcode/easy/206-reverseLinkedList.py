94026a3ad0395aa01ed50e39dbb2e0cb5f60d22d
nickyjhong <nicolehong95@gmail.com>
2022-12-13 13:15:11 -0500

    leetcode python 121 206 412

---
+++ leetcode/easy/206-reverseLinkedList.py
@@ -0,0 +1,12 @@
+class Solution:
+  def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    prev = None
+    current = head
+
+    while current:
+      next = current.next
+      current.next = prev
+      prev = current
+      current = next
+    
+    return prev
