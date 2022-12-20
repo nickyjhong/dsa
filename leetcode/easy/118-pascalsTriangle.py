a659498e60f0889a8a1e9299678062ce98be1074
nickyjhong <nicolehong95@gmail.com>
2022-12-20 12:20:32 -0500

    lc python pascals triangle

---
+++ leetcode/easy/118-pascalsTriangle.py
@@ -0,0 +1,12 @@
+def generate(numRows):
+  result = [[1]]
+
+  for i in range(numRows - 1):
+    temp = [0] + result[-1] + [0]
+    row = []
+    for j in range(len(result[-1]) + 1):
+      row.append(temp[j] + temp[j + 1])
+
+    result.append(row)
+    
+  return result
