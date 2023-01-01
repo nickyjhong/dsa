3147e10452b0893f104b1b83b15f6f58c02f99c2
nickyjhong <nicolehong95@gmail.com>
2023-01-01 18:24:46 -0500

    lc 290 wordPattern

---
+++ leetcode/easy/290-wordPattern.py
@@ -0,0 +1,19 @@
+def wordPattern(pattern, s):
+  words = s.split(' ')
+  if len(pattern) != len(words):
+    return False
+
+  charToWord = {}
+  wordToChar = {}
+
+  for char, word in zip(pattern, words):
+    if char in charToWord and charToWord[char] != word:
+      return False
+
+    if word in wordToChar and wordToChar[word] != char:
+      return False
+
+    charToWord[char] = word
+    wordToChar[word] = char
+  
+  return True
