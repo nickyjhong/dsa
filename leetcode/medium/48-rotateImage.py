cf84b0ed8519c499c58aa7f4bdde0e5ec315e687
nickyjhong <nicolehong95@gmail.com>
2023-01-11 20:14:25 -0500

    lc 48 rotateImage

---
+++ leetcode/medium/48-rotateImage.py
@@ -0,0 +1,15 @@
+def rotate(matrix):
+  left, right = 0, len(matrix) - 1
+
+  while left < right:
+    for i in range(right - left):
+      top, bottom = left, right
+
+      topLeft = matrix[top][left + i]
+      matrix[top][left + i] = matrix[bottom - i][left]
+      matrix[bottom - i][left] = matrix[bottom][right - i]
+      matrix[bottom][right - i] = matrix[top + i][right]
+      matrix[top + i][right] = topLeft
+    
+    right -= 1
+    left += 1
